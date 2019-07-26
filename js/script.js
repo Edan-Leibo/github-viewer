20faec07056413ce2f6d46296efb109302ecc076
Edan <edan.leibo@gmail.com>
2019-07-26 23:23:26 +0300

    add styling

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const AMOUNT_OF_USERS = 15;
 
-const userSearchFieldUI = document.querySelector(".user-search-field");
-const searchButtonUI = document.querySelector(".user-search-submit");
+const userSearchFieldUI = document.querySelector(".search-group--search-field");
+const searchButtonUI = document.querySelector(".search-group--search-button");
 const mainContentUI = document.querySelector(".main-content");
+const usersListUI = document.querySelector(".users-list");
 const headerUI = document.querySelector("header")
 
 searchButtonUI.addEventListener("click", () => {
     fetch(`https://api.github.com/search/users?q=${userSearchFieldUI.value}`)
         .then(response => response.json())
         .then(res => {
-            renderEmptyUsersList();
             renderUsersList(res.items);
         })
         .catch(error => console.error(error));
@@ -17,27 +17,25 @@ searchButtonUI.addEventListener("click", () => {
 
 
 // UI for search screen
-function renderEmptyUsersList() {
-    mainContentUI.innerHTML = `
-    <li class="users-list">
-    </li>
-    `
-}
 
 function renderUsersList(usersList) {
+    usersListUI.innerHTML = "";
     usersList.forEach((user) => {
-        const usersListUI = document.querySelector(".users-list");
         usersListUI.appendChild(createUserElement(user));
     });
 }
 
 function createUserElement(user) {
     const userListItem = document.createElement("li");
+    userListItem.classList.add("user-card");
+    userListItem.classList.add("card");
     userListItem.innerHTML =
-        `<img src="${user.avatar_url}" alt="login name">
-        <h3>${user.login}</h3>
-        <a href="#" onclick="renderRepos('${user.repos_url}','${user.login}','${user.avatar_url}')">Repositories</a>
-        <a href = "${user.html_url}" target="_blank">Full Profile </a>`
+        `<img class="user-card--avatar-img" src="${user.avatar_url}" alt="user photo" >
+        <div>
+            <h3 class="user-card--login">${user.login}</h3>
+            <a class="btn user-card--repo-btn" href="#" onclick="renderRepos('${user.repos_url}','${user.login}','${user.avatar_url}')">Repositories</a>
+            <a class="btn user-card--profile-btn" href = "${user.html_url}" target="_blank">Full Profile </a>
+        </div>`
     return userListItem;
 }
 
@@ -47,28 +45,34 @@ function renderRepos(reposURL, userName, userAvatar) {
         .then(response => response.json())
         .then(res => {
             headerUI.innerHTML =
-                `<img src = "${userAvatar}" alt = "user photo" >            
-                <div>${userName}'s Repositories (${reposURL.length})<div>`;
+                `<img class="header--user-image" src="${userAvatar}" alt="user photo" >            
+                <h1 class="header--user-title">${userName}'s Repositories (${reposURL.length})</h1>`;
             renderReposList(res);
         })
         .catch(error => console.error(error));
 }
 
-
 function renderReposList(reposList) {
-    mainContentUI.innerHTML = "";
+    mainContentUI.innerHTML = '';
+    const reposListUI = document.createElement('ul');
+    reposListUI.classList.add('items-list');
+    reposListUI.classList.add('repos-list');
+    mainContentUI.appendChild(reposListUI);
+
     reposList.forEach((repo) => {
-        mainContentUI.appendChild(createRepoElement(repo));
+        reposListUI.appendChild(createRepoElement(repo));
     });
 }
 
 function createRepoElement(repo) {
     const repoListItem = document.createElement("li");
+    repoListItem.classList.add("card");
+    repoListItem.classList.add("repo-card");
     repoListItem.innerHTML =
-        `<h3> ${repo.full_name}</h3 >
-        <div class="privacy"> ${repo.private ? "Private" : "Public"}</div>
-        <div class="forks"> Forks:${repo.forks_count}</div>
-        <div class="watchers"> Watchers:${repo.watchers_count}</div>
-        <div class="url"> Url:${repo.html_url}</div>`
+        `<h3 class="repo-card--full-name"> ${repo.full_name}</h3>
+        <div class="repo-card--privacy"> ${repo.private ? "Private" : "Public"}</div>
+        <div class="repo-card--forks"> Forks: ${repo.forks_count}</div>
+        <div class="repo-card--watchers"> Watchers: ${repo.watchers_count}</div>
+        <div class="repo-card--url"> Url: <a href="${repo.html_url}">${repo.html_url}</a></div> `
     return repoListItem;
 }
